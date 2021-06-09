ff8b5428310cd3f7a7dcf0e0f81f4e92b4710f87
bhall20 <brianhall401@yahoo.com>
2021-06-08 23:57:56 -0400

    Added stripped and lowercase method

--- word_frequency.py
+++ word_frequency.py
@@ -1,19 +1,26 @@
+
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
 
-
+# Why isnt this printing? Is everything else functioning?
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
-    pass
-
+    a = []
+    with open('one-today.txt', 'r') as file:
+        file.readlines()
+        file.strip('.!,:;-"')
+        file.lower()
+        for line in file:
+            a.append(line)
+    print(a)
 
 if __name__ == "__main__":
     import argparse
     from pathlib import Path
-
     parser = argparse.ArgumentParser(
         description='Get the word frequency in a text file.')
     parser.add_argument('file', help='file to read')
